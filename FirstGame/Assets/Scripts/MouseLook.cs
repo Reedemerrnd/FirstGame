1d041a43ce809eb11023b4993452b5a34ce42eb5
Roman Avdeev <reedemer@yandex.ru>
2021-03-09 14:17:13 +0300

    Added Menu

--- FirstGame/Assets/Scripts/MouseLook.cs
+++ FirstGame/Assets/Scripts/MouseLook.cs
@@ -5,19 +5,25 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
     
-    
+   
     [SerializeField]
-    private Camera _cam;
+    private float _sensivityHor = 10.0f;
     [SerializeField]
-    private float _sensivityHor = 9.0f;
-    [SerializeField]
-    private float _sensivityVer = 9.0f;
-    
+    private float _sensivityVer = 10.0f;
+    public float SensivityX { get=>_sensivityHor; set=>_sensivityHor = value; }
+    public float SensivityY { get => _sensivityVer; set => _sensivityVer = value; }
+    private Transform _camJoint;
+    private Transform _weaponJoint;
     private float minVert = -90.0f;
     private float maxVert = 90.0f;
 
     private float _rotationX = 0;
 
+    private void Start()
+    {
+        _camJoint = transform.Find("CamJoint");
+        _weaponJoint = transform.Find("JointWeap");
+    }
 
     // Update is called once per frame
     void Update()
@@ -29,7 +35,10 @@ public class MouseLook : MonoBehaviour
             float delta = Input.GetAxis("Mouse X") * _sensivityHor;
             float rotationY = transform.localEulerAngles.y + delta;
             transform.localEulerAngles = new Vector3(0, rotationY, 0);
-            _cam.transform.localEulerAngles = new Vector3(_rotationX, 0, 0);
-       
+            _camJoint.transform.localEulerAngles = new Vector3(_rotationX, 0, 0);
+            _weaponJoint.transform.localEulerAngles = new Vector3(_rotationX, 0, 0);
+        //_cam.transform.localEulerAngles = new Vector3(_rotationX, 0, 0);
+
     }
+
 }
